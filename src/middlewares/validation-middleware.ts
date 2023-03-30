1e973ee3105cd6a18fbcf3986a1090cfa289cff7
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-03-30 01:41:07 -0300

    feat: middleware validation bady sign in

--- src/middlewares/validation-middleware.ts
+++ src/middlewares/validation-middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { createUserSchema } from "../schemas/users-schemas";
+import { signInSchema } from "../schemas/authentication-schemas";
 
 export function validateBody(req: Request, res: Response, next: NextFunction) {
   const user = req.body;
@@ -14,19 +15,22 @@ export function validateBody(req: Request, res: Response, next: NextFunction) {
   next();
 }
 
-// export function validateBodySignIn(
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) {
-//   const user = req.body;
+export function validateBodySignIn(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const user = req.body;
 
-//   const { error } = signInSchema.validate(user, { abortEarly: false });
+  
 
-//   if (error) {
-//     const errors = error.details.map((d) => d.message);
-//     return res.status(400).send(errors);
-//   }
+  const { error } = signInSchema.validate(user, { abortEarly: false });
 
-//   next();
-// }
+  if (error) {
+    
+    const errors = error.details.map((d) => d.message);
+    return res.status(400).send(errors);
+  }
+
+  next();
+}
--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { validateBodySignIn } from "../middlewares/validation-middleware";
 import { signInPost } from "../controllers/authentication-controller";
 
 const authenticationRouter = Router();
 
-authenticationRouter.post("/sign-in", signInPost);
+authenticationRouter.post("/sign-in", validateBodySignIn, signInPost);
 
 export {authenticationRouter};
